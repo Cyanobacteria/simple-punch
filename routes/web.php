6f4e09e9b3738fc00c45712ddde756297e4a7ff3
water <azsx96385@gmmail.com>
2019-11-20 10:46:47 +0800

    done

--- routes/web.php
+++ routes/web.php
@@ -17,11 +17,12 @@ Auth::routes();
 
 Route::get('/', 'IndexController@index');
 Route::get('/home', 'HomeController@index')->name('home');
-//check the punch record
+//員工檢視-月份打卡紀錄
 Route::get('/read', 'HomeController@read')->name('read.get');
 Route::post('/read', 'HomeController@read')->name('read.post');
-
+//員工打卡
 Route::post('/work', 'WokerController@store')->name('Worker.store');
+
 
 //------------
 Route::get('/ok', 'WokerController@index');
